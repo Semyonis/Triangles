595848763e5aa1f14809b4b34856bfb9dae6b833
simeoniys <simeoniys@gmail.com>
2024-08-12 14:32:29 +0700

    added RightTriangleAccuracyTests

---
+++ TrianglesTests/RightTriangleAccuracyTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Triangles;
+using Xunit;
+
+namespace TrianglesTests;
+
+
+public sealed class RightTriangleAccuracyTests
+{
+    [Theory]
+    [InlineData(5, 2.99999, 4)]
+    public void AcuteTriangleTest(
+        double sideA,
+        double sideB,
+        double sideC
+    )
+    {
+        var triangleService =
+            new TriangleService();
+
+        var type =
+            triangleService
+                .GetType(
+                    sideA,
+                    sideB,
+                    sideC
+                );
+
+        type
+            .Should()
+            .Be(TriangleType.Right);
+    }
+}
